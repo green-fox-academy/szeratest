636f6033481cb1bd4c9b6b69970cb836a016b26c
vdominika <vdominika@yahoo.com>
2017-11-28 15:48:04 +0100

    c6

--- src/Count.java
+++ src/Count.java
@@ -1,7 +1,6 @@
 public class Count {
 
-    public void count(String mikorka, String kalman) {
-        System.out.println(mikorka);
-        System.out.println(kalman);
+    public void count(String mikorka) {
+        System.out.println("Counting the countless"+ mikorka);
     }
 }
