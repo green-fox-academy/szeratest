681e7ebadfc556d33725205aac39a453943ccae5
pkrisz0 <p.krisz89@gmail.com>
2017-11-28 10:56:37 +0100

    a2

--- src/GreetKrisz.java
+++
@@ -1,5 +0,0 @@
-public class GreetKrisz {
-    public static void greet(String name){
-        System.out.println(name);
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        greet("Kriszti");
+    }
+
+    public static void greet(String name){
+        System.out.println(name);
+    }
+}
+
